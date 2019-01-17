10a84ba1db69792993f8415f6400b05b55a96de6
Luzi82 <luzi82@gmail.com>
2019-01-17 21:58:09 +0800

    add django example

---
+++ django/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+echo sudo for right
+
+sudo true
+
+rm -rf venv
+sudo rm -rf composeexample
+rm -rf manage.py
+
+virtualenv -p `which python3` venv
+source venv/bin/activate
+pip install docker-compose
+
+docker-compose run web django-admin.py startproject composeexample .
+
+sudo chown -R $USER:$USER .
+
+patch -p0 < settings.py.patch
+
+docker-compose up -d
+
+sleep 1
+
+curl http://localhost:8000
+
+docker ps
+
+docker-compose down
